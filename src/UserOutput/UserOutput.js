b057e42eab1e7f4fe60ab8f2bbedd2babc1cd80b
bojanche <Boj@n@25122015>
2020-12-17 11:52:15 +0100

    initial release

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = (props) => {
+    return (
+        <div>
+            <p>This is just a test! User: {props.user_name}</p>
+            <p>Diese is eine test!</p>
+        </div>
+    )
+};
+
+export default userOutput;
